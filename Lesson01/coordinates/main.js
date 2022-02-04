a55de266592a83e55848eb07d793e9fd32cb510b
hyejun-an <hyejunan.canada@gmail.com>
2022-02-04 13:57:06 -0500

    Create find a rabbits game

--- Lesson01/coordinates/main.js
+++ Lesson01/coordinates/main.js
@@ -1,12 +1,17 @@
-const coord = document.querySelector('.coord');
-function updateCoord(e) {
-    coord.innerHTML = `
-    Coords : ${e.clientX}, ${e.clientY}
-    `
-    coord.style.top = e.clientX + 'px';
-    coord.style.left = e.clientY + 'px';
-}
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
 
-window.addEventListener('mousemove', (e) => {
-    updateCoord(e);
-});
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.innerHTML = `${x}px, ${y}px`;
+})

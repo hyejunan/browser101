11ca8ee735821a8aa5f1a80a9090b7d1fb0a74fe
hyejun-an <hyejunan.canada@gmail.com>
2022-02-05 08:58:13 -0500

    Optimize coordinates code
    change left, top to transform

--- Lesson01/coordinates/main.js
+++ Lesson01/coordinates/main.js
@@ -3,15 +3,27 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width /2;
+    const targetHalfHeight = targetRect.height / 2;
+    // optimize coordinate code 
+    // operation : layout -> paint -> composition
+    // It's better to use translate than left/top 
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.innerHTML = `${x}px, ${y}px`;
+        //vertical.style.left = `${x}px`;
+        vertical.style.transform = `translateX(${x}px)`;
+        //horizontal.style.top = `${y}px`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        //target.style.left = `${x}px`;
+        //target.style.top = `${y}px`;
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+        //tag.style.left = `${x}px`;
+        //tag.style.top = `${y}px`;
+        tag.style.transform = `translate(${x+20}px, ${y+20}px)`;
+        tag.innerHTML = `${x}px, ${y}px`;
+    })
 })

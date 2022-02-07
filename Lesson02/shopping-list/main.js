7bd9f8224206479d4a5a327789df0963339dec46
hyejun-an <hyejunan.canada@gmail.com>
2022-02-06 20:40:13 -0500

    shopping list

---
+++ Lesson02/shopping-list/main.js
@@ -0,0 +1,7 @@
+let today = new Date();
+const date = document.querySelector(".date");
+console.log(today);
+console.log(date);
+window.addEventListener('load', () => {
+   date.innerHTML = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+})

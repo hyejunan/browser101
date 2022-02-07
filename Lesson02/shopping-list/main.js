578eb1c70f848e70e9c4f5ae30b15f892de333e2
hyejun-an <hyejunan.canada@gmail.com>
2022-02-07 15:35:43 -0500

    solution

--- Lesson02/shopping-list/main.js
+++ Lesson02/shopping-list/main.js
@@ -5,3 +5,19 @@ console.log(date);
 window.addEventListener('load', () => {
    date.innerHTML = today.getFullYear() + '/' + ('0' + (today.getMonth() + 1)).slice(-2) + '/' + ('0' + today.getDate()).slice(-2);
 })
+
+let input = querySelector(".input-box");
+let addBtn = querySelector(".add-btn");
+let ul = querySelector("ul");
+
+addBtn.addEventListener('click', () => {
+    ul.innerHTML = `
+        <li>
+        <label class="container">
+            <input type="checkbox" id="check">
+            ${input}
+        </label>
+        <i class="far fa-trash-alt"></i>
+        </li>
+    `
+});
---
+++ Lesson02/shopping-list/solution/main.js
@@ -0,0 +1,61 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+
+function onAdd() {
+    // 1. get input text
+    const text = input.value;
+    if(text === '') {
+        input.focus();
+        return;
+    }
+    console.log(text);
+    // 2. create new item(text+deletebtn)
+    const item = createItem(text);
+    // 3. add new item to items container
+    items.appendChild(item);
+    // 4. scroll to new item
+    item.scrollIntoView({block: 'center'});
+    // 5. initialize input box
+    input.value = '';
+    input.focus();
+}
+
+function createItem(text) {
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item__row');
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item');
+
+    const name = document.createElement('span');
+    name.setAttribute('class', 'item__name');
+    name.innerText = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+    deleteBtn.addEventListener('click', () => {
+        items.removeChild(itemRow);
+    })
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    item.appendChild(name);
+    item.appendChild(deleteBtn);
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+    return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+    onAdd();
+})
+
+input.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter')
+        onAdd();
+})
+

b7f09bde051b54f48e99a6548c8f49f042e40114
hyejun-an <hyejunan.canada@gmail.com>
2022-02-06 21:05:30 -0500

    Add css

--- Lesson02/shopping-list/main.js
+++ Lesson02/shopping-list/main.js
@@ -3,5 +3,5 @@ const date = document.querySelector(".date");
 console.log(today);
 console.log(date);
 window.addEventListener('load', () => {
-   date.innerHTML = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+   date.innerHTML = today.getFullYear() + '/' + ('0' + (today.getMonth() + 1)).slice(-2) + '/' + ('0' + today.getDate()).slice(-2);
 })

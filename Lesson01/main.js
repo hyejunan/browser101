7bd8b894f9c6341dadf0268ba1f7242db947b8dc
hyejun-an <hyejunan.canada@gmail.com>
2022-02-08 18:57:09 -0500

    Update main.js
    add object explanation

--- Lesson01/main.js
+++ Lesson01/main.js
@@ -5,3 +5,16 @@ console.log(num);
 
 num = 3;
 console.log(num);
+
+// object : array, function, list ...
+
+const obj = {
+    name: 'ellie',
+    age: 5,
+};
+let obj2 = obj;
+console.log(obj2.name); // ellie
+
+obj.name = 'james'
+console.log(obj.name); // james
+console.log(obj2.name); // james

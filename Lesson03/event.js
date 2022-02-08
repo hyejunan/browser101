4b2ee3e3e8c1425e8261dcbdd19781c2761b022b
hyejun-an <hyejunan.canada@gmail.com>
2022-02-08 07:55:27 -0500

    Add lesson3

---
+++ Lesson03/event.js
@@ -0,0 +1,15 @@
+/*
+    Types of Event
+    - mouse click
+    - keyboard
+    - resizing window
+    - close window
+    - page loading
+    - form submission
+    - video is being played
+    - error
+
+    addEventListener()
+    removeEventListner()
+    dispatchEvent()
+*/

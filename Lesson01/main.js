194f65896bc59f78e7a61d7f51951e6a9dfa4ac8
hyejun-an <hyejunan.canada@gmail.com>
2022-02-08 19:18:56 -0500

    Update main.js
    add function explanation

--- Lesson01/main.js
+++ Lesson01/main.js
@@ -18,3 +18,33 @@ console.log(obj2.name); // ellie
 obj.name = 'james'
 console.log(obj.name); // james
 console.log(obj2.name); // james
+
+// function
+function add(a,b) {
+    return a+b;
+}
+
+const sum = add(3,4);
+
+const doSomething = add;
+
+const result = doSomething(2,3); //5
+const result2 = add(2,3);//5
+
+function print() {
+    console.log('print');
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function surprise(operator) {
+    const result = operator(2,3); // add(2,3)
+    console.log(result);
+}
+
+surprise(); // error(operator undefined)
+surprise(add); // NaN
+surprise(divide);
+

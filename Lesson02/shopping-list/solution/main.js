62502e0a47b648888a702a877662244973f90192
hyejun-an <hyejunan.canada@gmail.com>
2022-02-11 13:23:12 -0500

    Update main.js

--- Lesson02/shopping-list/solution/main.js
+++ Lesson02/shopping-list/solution/main.js
@@ -1,4 +1,5 @@
 const items = document.querySelector('.items');
+const footer = document.querySelector('footer');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 

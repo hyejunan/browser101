128bf74839a3a17264eea716044c8af8c43b3ae4
hyejun-an <hyejunan.canada@gmail.com>
2022-02-17 19:37:30 -0500

    Update main.js
    Add game timer

--- Lesson04/carrot/main.js
+++ Lesson04/carrot/main.js
@@ -2,6 +2,7 @@
 const CARROT_SIZE = 80;
 const CARROT_CNT = 5;
 const BUG_CNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
@@ -31,6 +32,24 @@ function startGame() {
 
 function stopGame() {
 
+}
+
+function startGameTimer() {
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+    timer = setInterval(() => {
+        if(remainingTimeSec <= 0) {
+            clearInterval(timer);
+            return;
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000);
+}
+
+function updateTimerText(time) {
+    const minutes = Math.floor(time/60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
 }
 
 function showStopButton() {

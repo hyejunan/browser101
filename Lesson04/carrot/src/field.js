733b0c6985a640ef91a84d312f4ff3dadd6bfe32
hyejun-an <hyejunan.canada@gmail.com>
2022-02-22 21:22:25 -0500

    Update field.js
    this binding update

--- Lesson04/carrot/src/field.js
+++ Lesson04/carrot/src/field.js
@@ -11,8 +11,9 @@ export default class Field {
         this.bugCount = bugCount;
         this.field = document.querySelector('.game__field');
         this.fieldRect = this.field.getBoundingClientRect();
-        this.onClick = this.onClick.bind(this);
-        this.field.addEventListener('click', this.onClick);
+        //this.onClick = this.onClick.bind(this);
+        //this.field.addEventListener('click',(event) => this.onClick(event));
+        this.field.addEventListener('click',this.onClick);
     }
 
     init() {
@@ -44,7 +45,7 @@ export default class Field {
         }
     }
 
-    onClick(e) {
+    onClick = e =>  {
         const target = e.target;
         if(target.matches('.carrot')) {
             target.remove();
